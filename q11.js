3cf58c651c42c3797e6ff41ebcc316d699d36e93
Vishnumaya <vm6553088@gmail.com>
2024-08-28 09:46:25 +0530

    q11

---
+++ q11.js
@@ -0,0 +1,12 @@
+/**
+ * @param {Array} arr
+ * @param {Function} fn
+ * @return {Array}
+ */
+var sortBy = function(arr, fn) {
+    return arr.sort((a,b)=>{
+        return fn(a)-fn(b)
+    })
+};
+console.log(sortBy(arr = [5, 4, 1, 2, 3], fn = (x) => x));
+
