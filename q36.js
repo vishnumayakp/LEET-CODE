59d54b06065345e7cb07a90539125f16ed3bdd8a
Vishnumaya <vm6553088@gmail.com>
2024-10-01 09:53:51 +0530

    q36

---
+++ q36.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string} num1
+ * @param {string} num2
+ * @return {string}
+ */
+var addStrings = function(num1, num2) {
+    let value1=Number(num1)
+    let value2=Number(num2)
+    let result=value1+value2 
+    return String(result)
+    
+};
+console.log(addStrings("11", "123"));
+console.log(addStrings("456", "77"));
+console.log(addStrings("9333852702227987", "85731737104263"));
+
