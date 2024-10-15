a96e35c2dffedd06ca0abad8dbc047c80f48a8ed
Vishnumaya <vm6553088@gmail.com>
2024-10-15 09:48:54 +0530

    q45

---
+++ q45.js
@@ -0,0 +1,13 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var toLowerCase = function(s) {
+   return s.toLowerCase()
+   
+};
+console.log(toLowerCase("Hello"));
+console.log(toLowerCase("here"));
+console.log(toLowerCase("LOVELY"));
+
+
