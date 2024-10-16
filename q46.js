30556aeb8f48f01386628ada86f961f112f16311
Vishnumaya <vm6553088@gmail.com>
2024-10-16 09:38:01 +0530

    q46

---
+++ q46.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var mySqrt = function(x) {
+    let value=Math.sqrt(x)
+    return Math.floor(value)
+};
+console.log(mySqrt(4));
+console.log(mySqrt(8));
+
