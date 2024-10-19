d8f09990a282311ff2c45ac5e72a41f02d9925e5
Vishnumaya <vm6553088@gmail.com>
2024-10-19 10:07:26 +0530

    q49

---
+++ q49.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isPowerOfFour = function(n) {
+    if(n<=0) return false
+    while (n%4===0){
+        n/=4
+    }
+    return n===1
+};
+console.log(isPowerOfFour(16));
+console.log(isPowerOfFour(5));
+console.log(isPowerOfFour(1));
+
+
