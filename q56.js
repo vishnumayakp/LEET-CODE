be92a6af063377927a8696049fe58e1ee14d2fe7
Vishnumaya <vm6553088@gmail.com>
2024-10-29 09:52:05 +0530

    q56

---
+++ q56.js
@@ -0,0 +1,15 @@
+/**
+ * @param {string} s
+ * @param {number[]} indices
+ * @return {string}
+ */
+var restoreString = function(s, indices) {
+    let result=[];
+    for(let i=0; i<s.length; i++){
+       result[indices[i]]=s[i] 
+    }
+    return result.join('')
+};
+console.log(restoreString("codeleet",[4,5,6,7,0,2,1,3]));
+console.log(restoreString("abc",[0,1,2]));
+
