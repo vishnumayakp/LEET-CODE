ca2fb46a699430c38da875bd146651202809c1d3
VISHNUMAYA KP <103446525+vishnumayakp@users.noreply.github.com>
2024-11-07 10:46:05 +0530

    Time: 0 ms (100%), Space: 27 MB (35.07%) - LeetHub

---
+++ 1406-subtract-the-product-and-sum-of-digits-of-an-integer/1406-subtract-the-product-and-sum-of-digits-of-an-integer.cs
@@ -0,0 +1,12 @@
+public class Solution {
+    public int SubtractProductAndSum(int n) {
+        int sum=0,product=1;
+    while(n>0){
+        int digit=n%10;
+        sum+=digit;
+        product*=digit;
+         n=n/10;
+    }
+    return product-sum;
+    }
+}
